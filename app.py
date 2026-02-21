7356be8d67bae78e15ecd97b027c976b96c7daaf
diegiyo138@gmail.com <diegiyo138@gmail.com>
2026-02-20 18:32:41 -0600

    detalles 20/02/2026

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from sqlalchemy.ext.asyncio import create_async_engine
 from flask import Flask, render_template, request, redirect, url_for
 from flask import flash
 from flask_wtf.csrf import CSRFProtect
@@ -23,9 +24,51 @@ def index():
     alumnos_list = Alumnos.query.all()
     return render_template("index.html", form=create_form, alumnos=alumnos_list)
 
-@app.route("/Alumno")
+@app.route("/Alumno", methods=['GET', 'POST'])
 def alumnos():
-	return render_template("alumnos.html")
+	create_form=forms.UserForm(request.form)
+	if request.method == 'POST':
+		alum=Alumnos(nombre=create_form.nombre.data,
+		apaterno=create_form.apaterno.data,
+		email=create_form.email.data)
+		db.session.add(alum)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template("alumnos.html", form=create_form)
+
+@app.route("/detalles", methods=['GET', 'POST'])
+def detalles():
+    id = request.args.get('id')
+    alumno1 = Alumnos.query.get(id)
+    if alumno1:
+        nombre = alumno1.nombre
+        apaterno = alumno1.apaterno
+        email = alumno1.email
+        return render_template("detalles.html", id=id, nombre=nombre, apaterno=apaterno, email=email)
+    return redirect(url_for('index'))
+
+@app.route("/modificar", methods=["GET", "POST"])
+def modificar():
+    id = request.args.get('id')
+    alum = Alumnos.query.get(id)
+    create_form = forms.UserForm(request.form, obj=alum)
+    if request.method == 'POST' and create_form.validate():
+        alum.nombre = create_form.nombre.data
+        alum.apaterno = create_form.apaterno.data
+        alum.email = create_form.email.data
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template("modificar.html", form=create_form)
+
+@app.route("/eliminar", methods=["GET", "POST"])
+def eliminar():
+    id = request.args.get('id')
+    alum = Alumnos.query.get(id)
+    if request.method == 'POST':
+        db.session.delete(alum)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template("eliminar.html", alum=alum)
 
 if __name__ == '__main__':
 	csrf.init_app(app)
